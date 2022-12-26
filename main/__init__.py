631e92c684a07bd5884edbb1da69d1abada49248
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2022-12-26 20:13:46 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,10 +14,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 20869850
 API_HASH = "b9d94e303d86df2a2b32679e50ccfb43"
-BOT_TOKEN = "5713314297:AAEV5vDP5rQPOMdX3QUHugoCYJ4_EDe1mhY"
+BOT_TOKEN = "5872611768:AAGo6x9F_Fz4JiOR1OdwM1uqEm9G2h3z4zs"
 SESSION = "AQBPUMVJICRDJOb-oGUlTACu6CF3XbLmZ85NjcwHapyW-1bo6sozw9fKpNJ25xeEAEVlRjqCqdQsuZzFp4wYIfDkL8caLL7WlI1hFDLcybQB1aqpDZCTZNz21aBW2ClT_yhydAs1l4Lq7xU4ctjlHty2ttJdj1BurhNDjUpnp7sOE_jxyrdJ8g5E72-EPD1EOnLzREbT87394PvH-G9QRfUbIs-uZtrA1ixRBADbl6dhF1185wJX7BxaHm8wOUqVIKxHfF3br5bUOqXcLCOyBidyuWUCDsk44_p6JIe8456ADWqJb2AwalK4aJeWnoF-nQpYrF9AbAHLCBjshYxRjsG5AAAAAVLxVIIA"
-FORCESUB = "forcesubbbbbbbbdkkd"
-AUTH = 5531461861
+FORCESUB = "kp_linkz"
+AUTH = 5422893943 5531461861
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
